0aa151245cc2d21e362e45fbdc7ab28deb230c2f
Jordan Janzen <jordancjanzen@gmail.com>
2017-08-10 12:04:43 -0600

    Remove (as-of-yet) unused routers

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, HashRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import "../theme/App.css";
 
