da1e4b6165e4b55a0e73945aadd7ce82cd641807
Jordan Janzen <jordancjanzen@gmail.com>
2017-08-10 11:51:17 -0600

    Add container folder and get that all working

---
+++ src/containers/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Route, HashRouter, Link } from "react-router-dom";
+import logo from "../logo.svg";
+import "../theme/App.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { IndexRoute, Router, Route, hashHistory } from "react-router";
+import { Route, HashRouter, Link } from "react-router-dom";
 import App from "./containers/App";
 import Home from "./containers/Home";
 
 export default (
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-    </Route>
-  </Router>
+  <HashRouter>
+    <Route exact path="/" component={Home} />
+  </HashRouter>
 );
