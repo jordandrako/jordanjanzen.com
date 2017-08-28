b6b1392e23d6cf53807f4e7064a305ea9d39271d
Jordan Janzen <jordancjanzen@gmail.com>
2017-08-28 16:57:42 -0600

    Add mobile/large hidden styles, may remove when I learn more about styled-components

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -7,4 +7,15 @@ injectGlobal`
     font-family: 'VT323', monospace;
     font-size: 24px;
   }
+  .large {
+    display: hidden;
+  }
+  @media only screen and (min-width: 800px) {
+    .mobile {
+      display: none;
+    }
+    .large {
+      display: block;
+    }
+  }
 `;
