ded7d2c7df2a947c9351be40bfb597c1c5e98bc4
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-09-03 13:00:58 -0600

    Some tagline

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 
-const App = () => (
-  <wrapper>
-    <Header tagline="Never. Stop. Learning." />
-    <Main />
-  </wrapper>
-);
+const App = () => {
+  const tagline = 'Never  Stop   Learning± ';
+
+  return (
+    <wrapper>
+      <Header tagline={tagline} />
+      <Main />
+    </wrapper>
+  );
+};
 
 export default App;
