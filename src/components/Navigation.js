63dd3f7d64ed98a5dbea42540a403962d597794b
Jordan Janzen <jordancjanzen@gmail.com>
2017-09-15 17:53:31 -0600

    Stateless nav component again. Start making login non-user facing.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 // import base from '../base';
 import { colors, typography } from '../theme/variables';
-import Button from './Button';
 
 const NavList = styled.ul`
   list-style: none;
@@ -41,102 +40,26 @@ const Link = styled(NavLink)`
   }
 `;
 
-class Navigation extends Component {
-  constructor() {
-    super();
-
-    // this.renderLogin = this.renderLogin.bind(this);
-    // this.logout = this.logout.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
-    // this.authenticate = this.authenticate.bind(this);
-    // this.authHandler = this.authHandler.bind(this);
-
-    this.state = {
-      uid: null,
-      owner: null,
-    };
-  }
-
-  componentDidMount() {
-    // base.onAuth((user) => {
-    //   if (user) {
-    //     this.authHandler(null, { user });
-    //   }
-    // });
-  }
-
-  // authenticate(provider) {
-  //   console.log(`Trying to log in with ${provider}`);
-  //   // base.authWithOAuthPopup(provider, this.authHandler);
-  // }
-
-  // logout() {
-  //   // base.unauth();
-  //   this.setState({ uid: null });
-  //   console.log('logout');
-  // }
-
-  // authHandler(err, authData) {
-  //   console.log(authData);
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-
-  // grab the store info
-  // const storeRef = base.database().ref(this.props.storeId);
-
-  // query the firebase once for the store data
-  // storeRef.once('value', (snapshot) => {
-  //   const data = snapshot.val() || {};
-
-  //   // claim it as our own if there is no owner already
-  //   if (!data.owner) {
-  //     storeRef.set({
-  //       owner: authData.user.uid,
-  //     });
-  //   }
-
-  //   this.setState({
-  //     uid: authData.user.uid,
-  //     owner: data.owner || authData.user.uid,
-  //   });
-  // });
-  // }
-  render() {
-    return (
-      <nav>
-        <NavList className={this.props.navType}>
-          <li>
-            <Link exact to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/todo">Todo</Link>
-          </li>
-          <li>
-            <Button
-              type="login"
-              arrows
-              onClick={() => {
-                this.authenticate('google');
-              }}
-            >
-              {!this.state.uid ? 'Log In' : 'Log Out'}
-            </Button>
-          </li>
-        </NavList>
-      </nav>
-    );
-  }
-}
+const Navigation = props => (
+  <nav>
+    <NavList className={props.navType}>
+      <li>
+        <Link exact to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/todo">Todo</Link>
+      </li>
+    </NavList>
+  </nav>
+);
 
 Navigation.propTypes = {
   navType: PropTypes.string.isRequired,
