9d2d270328fad56bf52c0bfa0d554bf18e9a910f
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-09-18 19:49:12 -0600

    Updated for new prop structure

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -2,50 +2,62 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextareaAutosize from 'react-autosize-textarea';
 
-import { colors } from '../theme/variables';
+import StyledForm from './StyledForm';
 import Button from './Button';
-import Form from './Form';
+import { colors } from '../theme/variables';
 
 class AddTodoForm extends Component {
-  createTodo(event) {
-    event.preventDefault();
+  constructor() {
+    super();
+    this.createTodo = this.createTodo.bind(this);
+  }
+
+  createTodo(e) {
+    e.preventDefault();
     const todo = {
       name: this.name.value,
-      cat: this.cat.value,
+      category: this.category.value,
       desc: this.desc.value,
       link: this.link.value,
-      status: 'incomplete',
+      complete: false,
     };
     this.props.addTodo(todo);
-    // this.todoForm.reset();
   }
 
   render() {
     return (
       <div>
         <h3>Add a new todo item</h3>
-        <Form
-          ref={input => (this.todoForm = input)}
-          className="add-todo"
-          onSubmit={e => this.createTodo(e)}
-        >
-          <input ref={input => (this.name = input)} type="text" placeholder="Todo" />
-          <select ref={input => (this.cat = input)} type="text" placeholder="Todo Category">
-            <option value="design">Design</option>
-            <option value="component">Component</option>
-            <option value="quality">Quality</option>
-            <option value="backend">Backend</option>
-          </select>
-          <TextareaAutosize
-            ref={input => (this.desc = input)}
-            type="text"
-            placeholder="Todo Desc"
-          />
-          <input ref={input => (this.link = input)} type="text" placeholder="Todo link" />
-          <Button type="submit" bg={colors.green}>
-            + Add Todo
-          </Button>
-        </Form>
+        <StyledForm>
+          <form
+            ref={input => (this.todoForm = input)}
+            className="add-todo"
+            onSubmit={e => this.createTodo(e)}
+          >
+            <input ref={input => (this.name = input)} type="text" placeholder="Todo" required />
+            <select
+              ref={input => (this.category = input)}
+              type="text"
+              placeholder="Todo Category"
+              required
+            >
+              <option>Select Category</option>
+              <option value="design">Design</option>
+              <option value="component">Component</option>
+              <option value="quality">Quality</option>
+              <option value="backend">Backend</option>
+            </select>
+            <TextareaAutosize
+              ref={input => (this.desc = input)}
+              type="text"
+              placeholder="Todo Desc"
+            />
+            <input ref={input => (this.link = input)} type="text" placeholder="Todo link" />
+            <Button type="submit" styleType="success" arrows={colors.lightwhite}>
+              + Add Todo
+            </Button>
+          </form>
+        </StyledForm>
       </div>
     );
   }
