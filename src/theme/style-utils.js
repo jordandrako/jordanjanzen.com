0a47827a4393290996ed56a189ce8b3f499395c7
Jordan Janzen <jordancjanzen@gmail.com>
2017-10-08 17:50:04 -0600

    Add style utils for media queries and truncation

---
+++ src/theme/style-utils.js
@@ -0,0 +1,28 @@
+import { css } from 'styled-components';
+
+export const sizes = {
+  giant: 1170,
+  desktop: 992,
+  tablet: 768,
+  phone: 448,
+};
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  const emSize = sizes[label] / 16;
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)};
+    }
+  `;
+  return accumulator;
+}, {});
+
+export function truncate(width) {
+  return `
+    width: ${width};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `;
+}
