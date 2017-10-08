bfcc2f79f539b57ccb7a7e606af52048b2fa38bc
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-10-07 19:00:40 -0600

    Make banner useable

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// import { goBack } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { colors, typography } from '../theme/variables';
@@ -17,8 +18,8 @@ const Alert = styled.div`
     }
     return colors.lightblack;
   }};
-  padding: 1em;
-  margin: 1.5em;
+  padding: 1em 2em 1em 1em;
+  margin: 0 0 1.5em;
   border-radius: 0.25em;
   border: inset solid 3px rgba(255, 255, 255, 0.5);
   font-family: ${typography.monospace};
@@ -40,20 +41,20 @@ const Alert = styled.div`
       width: 80%;
       height: 3px;
       background: ${colors.black};
-      transition: all .1s ease-in-out;
+      transition: all 0.1s ease-in-out;
       transform-origin: 50% 50%;
       left: 10%;
       top: 50%;
       margin-top: -1.5px;
     }
-
   }
 
-  h4, .actionButton {
+  h4,
+  .actionButton {
     height: auto;
     display: block;
     opacity: 1;
-    transition: all .5s ease-in-out;
+    transition: all 0.5s ease-in-out;
   }
 
   h4 {
@@ -61,15 +62,16 @@ const Alert = styled.div`
   }
 
   .actionButton {
-    margin: .5em 0 0;
+    margin: 0.5em 0 0;
     background: transparent;
     border: 2px solid ${colors.black};
     font-family: ${typography.monospace};
-    padding: .4em 1em;
+    padding: 0.4em 1em;
   }
 
   &.closed {
-    h4, .actionButton {
+    h4,
+    .actionButton {
       height: 0;
       display: none;
       opacity: 0;
@@ -86,28 +88,37 @@ class Banner extends Component {
   constructor() {
     super();
     this.showHide = this.showHide.bind(this);
-    this.doAction = this.doAction.bind(this);
+    this.doAction = this.handleAction.bind(this);
 
     this.state = {
       closed: false,
+      action: null,
     };
   }
 
   showHide() {
     this.setState({
       closed: !this.state.closed,
-    })
+    });
   }
 
-  doAction() {
+  handleAction() {
     const action = this.props.action;
+    if (typeof action === 'function') {
+      this.props.action();
+    }
     if (action === 'hide') {
-      console.log('hide')
+      console.log('hide');
       this.showHide();
     }
-    if (action === 'reload' || 'retry') {
+    if (action === 'reload') {
       console.log('forceUpdate');
       return this.forceUpdate();
+    }
+    if (action === 'back') {
+      // TODO: make router go back
+      console.log('Going back');
+      return this.context.router.goBack();
     }
     this.setState.action = 'Unknown action';
     return null;
@@ -116,8 +127,20 @@ class Banner extends Component {
   render() {
     const action = this.props.action;
     const closed = this.state.closed ? 'closed' : null;
-    const showHideButton = (<button className="showHide" onClick={this.showHide}><span className="horizontal" /><span className="vertical" /></button>)
-    const actionButton = (<button className="actionButton" onClick={action === 'hide' ? this.showHide : this.doAction}>{toTitleCase(this.props.action)}</button>);
+    const showHideButton = (
+      <button className="showHide" onClick={this.showHide}>
+        <span className="horizontal" />
+        <span className="vertical" />
+      </button>
+    );
+    const actionButton = (
+      <button
+        className="actionButton"
+        onClick={action === 'hide' ? this.showHide : this.handleAction}
+      >
+        {toTitleCase(this.props.actionText)}
+      </button>
+    );
 
     return (
       <Alert type={this.props.type} className={closed}>
@@ -133,12 +156,16 @@ class Banner extends Component {
 Banner.propTypes = {
   children: PropTypes.node.isRequired,
   type: PropTypes.string,
-  action: PropTypes.string,
+  action: PropTypes.oneOfType([
+    PropTypes.oneOf(['hide', 'reload', 'back']),
+    PropTypes.func,
+  ]).isRequired,
+  actionText: PropTypes.string,
 };
 
 Banner.defaultProps = {
   type: 'info',
-  action: 'hide',
+  actionText: 'Hide',
 };
 
 export default Banner;
