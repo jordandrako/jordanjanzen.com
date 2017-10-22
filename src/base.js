1d489f1f2699038244113fc53eeb88eb6d481ec2
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-10-22 12:44:01 -0600

    Have staging and production env

--- src/base.js
+++ src/base.js
@@ -1,13 +1,21 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
-const app = firebase.initializeApp({
+// eslint-disable-next-line
+const appProduction = firebase.initializeApp({
   apiKey: 'AIzaSyA7SAPJlv0S3xHWvb-UpUIC9CaVkOMLdFo',
   authDomain: 'jordan-janzen.firebaseapp.com',
   databaseURL: 'https://jordan-janzen.firebaseio.com',
 });
 
-export const database = firebase.database(app);
+// eslint-disable-next-line
+const appStaging = firebase.initializeApp({
+  apiKey: 'AIzaSyDwVQkapFHWKYRwXxOt8Sqmm18LiXet628',
+  authDomain: 'jordan-janzen-staging.firebaseapp.com',
+  databaseURL: 'https://jordan-janzen-staging.firebaseio.com',
+});
+
+export const database = firebase.database(appStaging);
 
 export const auth = firebase.auth();
 export const provider = new firebase.auth.GoogleAuthProvider();
