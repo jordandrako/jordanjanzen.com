fc6950a682350fa7ecdbb43a78c721574605ad2d
Jordan Janzen <v-jojanz@microsoft.com>
2018-05-03 13:23:38 -0700

    Add check-env script (requires check-env and dotenv packages)

--- src/base.js
+++ src/base.js
@@ -1,30 +1,27 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
+const {
+  REACT_APP_DATABASE,
+  REACT_APP_PROD_KEY,
+  REACT_APP_STAGING_KEY
+} = process.env;
+
 const production = {
-  apiKey: `${process.env.REACT_APP_PROD_KEY}`,
+  apiKey: `${REACT_APP_PROD_KEY}`,
   authDomain: 'jordan-janzen.firebaseapp.com',
   databaseURL: 'https://jordan-janzen.firebaseio.com'
 };
 
 const staging = {
-  apiKey: `${process.env.REACT_APP_STAGING_KEY}`,
+  apiKey: `${REACT_APP_STAGING_KEY}`,
   authDomain: 'jordan-janzen-staging.firebaseapp.com',
   databaseURL: 'https://jordan-janzen-staging.firebaseio.com'
 };
 
-const env = () => {
-  if (process.env.REACT_APP_DATABASE === 'production') {
-    return production;
-  }
-  return staging;
-};
-
-const app = firebase.initializeApp(env());
-
+const appDatabase = REACT_APP_DATABASE === 'production' ? production : staging;
+const app = firebase.initializeApp(appDatabase);
 export const database = firebase.database(app);
-
 export const auth = firebase.auth();
 export const provider = new firebase.auth.GoogleAuthProvider();
-
 export const base = Rebase.createClass(database);
