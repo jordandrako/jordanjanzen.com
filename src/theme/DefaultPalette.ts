862bc609a11ecd411da2ae16870ab36fa0527e2a
Jordan Janzen <jordancjanzen@gmail.com>
2018-05-06 16:21:03 -0700

    default palette

---
+++ src/theme/DefaultPalette.ts
@@ -0,0 +1,20 @@
+import { IPalette } from './theme.types';
+
+const DefaultPalette: IPalette = {
+  black: "#282c34",
+  blue: "#1167ae",
+  cyan: "#56b6c2",
+  darkblack: "#111215",
+  green: "#98c379",
+  grey: "#848d9f",
+  lightblack: "#848d9f",
+  lightblue: "#61afef",
+  lightwhite: "#E1E9F0",
+  lightyellow: "#F2C249",
+  magenta: "#c678dd",
+  red: "#e06c75",
+  white: "#D3DAE0",
+  yellow: "#d19a66"
+};
+
+export default DefaultPalette;
