0ab7947074d9ecacc244ecda14424799b3f36f4c
Jordan Janzen <v-jojanz@microsoft.com>
2018-05-07 18:09:49 -0700

    new media queries

--- src/theme/style-utils.ts
+++ src/theme/style-utils.ts
@@ -8,22 +8,68 @@ export const sizes = {
 };
 
 // iterate through the sizes and create a media template
-export const mediaMax = Object.keys(sizes).reduce((accumulator, label) => {
-  const emSize = sizes[label] / 16;
-  accumulator[label] = (args: TemplateStringsArray) => css`
-    @media (max-width: ${emSize}em) {
-      ${css(args)};
-    }
-  `;
-  return accumulator;
-}, {});
+// export const mediaMax = Object.keys(sizes).reduce((accumulator, label) => {
+//   const emSize = sizes[label] / 16;
+//   accumulator[label] = (args: TemplateStringsArray) => css`
+//     @media (max-width: ${emSize}em) {
+//       ${css(args)};
+//     }
+//   `;
+//   return accumulator;
+// }, {});
 
-export const mediaMin = Object.keys(sizes).reduce((accumulator, label) => {
-  const emSize = sizes[label] / 16;
-  accumulator[label] = (args: TemplateStringsArray) => css`
-    @media (min-width: ${emSize}em) {
-      ${css(args)};
+// export const mediaMin = Object.keys(sizes).reduce((accumulator, label) => {
+//   const emSize = sizes[label] / 16;
+//   accumulator[label] = (args: TemplateStringsArray) => css`
+//     @media (min-width: ${emSize}em) {
+//       ${css(args)};
+//     }
+//   `;
+//   return accumulator;
+// }, {});
+
+export const mediaMin = {
+  desktop: (...args) => css`
+    @media (min-width: ${sizes.desktop}) {
+      ${ css(...args)}
     }
-  `;
-  return accumulator;
-}, {});
+  `,
+  giant: (...args) => css`
+    @media (min-width: ${sizes.giant / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+  phone: (...args) => css`
+    @media (min-width: ${sizes.phone / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+  tablet: (...args) => css`
+    @media (min-width: ${sizes.tablet / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+}
+
+export const mediaMax = {
+  desktop: (...args) => css`
+    @media (max-width: ${sizes.desktop / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+  giant: (...args) => css`
+    @media (max-width: ${sizes.giant / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+  phone: (...args) => css`
+    @media (max-width: ${sizes.phone / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+  tablet: (...args) => css`
+    @media (max-width: ${sizes.tablet / 16}em) {
+      ${ css(...args)}
+    }
+  `,
+}
