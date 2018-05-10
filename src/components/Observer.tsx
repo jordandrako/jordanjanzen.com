cb80ef92be83d718264edb816f5e7354e965eb01
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2018-05-10 15:21:20 -0700

    Type observer

--- src/components/Observer.tsx
+++ src/components/Observer.tsx
@@ -1,64 +1,69 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
 
-class Observer extends Component {
+interface IObserverProps {
+  isVisible: boolean;
+  children?: any;
+}
+
+interface IObserverState {
+  hasBeenVisible: boolean;
+  isVisible: boolean;
+}
+
+class Observer extends React.Component<IObserverProps, IObserverState> {
+  private _io: IntersectionObserver | null;
+  private _container: Element;
+
   public constructor(props: any) {
     super(props);
+
+    this._io = null;
+
     this.state = {
-      isVisible: false,
       hasBeenVisible: false,
+      isVisible: false,
     };
-    this.io = null;
-    this.container = null;
   }
 
-  public componentDidMount():void {
-    this.io = new IntersectionObserver(([entry]) => {
+  public componentDidMount(): void {
+    this._io = new IntersectionObserver(([entry]) => {
       this.setState({ isVisible: entry.isIntersecting });
       if (this.state.isVisible) {
-        this.setState({ hasBeenVisible: true })
+        this.setState({ hasBeenVisible: true });
       }
     }, {});
-    this.io.observe(this.container);
+    this._io.observe(this._container);
   }
+
   public componentWillUnmount(): void {
-    if (this.io) {
-      this.io.disconnect();
+    if (this._io) {
+      this._io.disconnect();
     }
   }
 
-  public componentWillReceiveNewProps(newProps): void {
+  public componentWillReceiveNewProps(newProps: IObserverProps): void {
     if (!this.state.hasBeenVisible && newProps.isVisible) {
-      this.setState({ hasBeenVisible: true })
+      this.setState({ hasBeenVisible: true });
     }
   }
+
   public render(): JSX.Element {
     return (
       // we create a div to get a reference.
       // It's possible to use findDOMNode() to avoid
       // creating extra elements, but findDOMNode is discouraged
-      <div
-        ref={(div) => {
-          this.container = div;
-        }}
-      >
+      <div ref={div => (this._container = div as Element)}>
         {Array.isArray(this.props.children)
-          ? this.props.children.map((child) => child(this.state.isVisible, this.state.hasBeenVisible))
-          : this.props.children(this.state.isVisible, this.state.hasBeenVisible)}
+          ? this.props.children.map(child =>
+              child(this.state.isVisible, this.state.hasBeenVisible)
+            )
+          : this.props.children(
+              this.state.isVisible,
+              this.state.hasBeenVisible
+            )}
       </div>
     );
   }
-}
-
-Observer.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.any),
-    PropTypes.any
-  ])
-}
-
-Observer.defaultProps = {
-  children: null
 }
 
 export default Observer;
