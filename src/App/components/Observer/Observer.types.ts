719575167625b3ff81a1166cc4133226112e603d
Jordan Janzen <v-jojanz@microsoft.com>
2018-05-22 15:46:21 -0700

    Delete Observer

--- src/App/components/Observer/Observer.types.ts
+++
@@ -1,6 +0,0 @@
-import { TChildren } from '../../App.types';
-
-export interface IObserverProps {
-  isVisible: boolean;
-  children?: TChildren;
-}
--- src/App/components/Observer/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './Observer';
-export * from './Observer.types';
