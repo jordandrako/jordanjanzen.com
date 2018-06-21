daa1a41d3ee8d1b546e3ffb0112f44d9f7cf9dcf
Jordan Janzen <jordancjanzen@gmail.com>
2018-06-20 17:51:22 -0700

    Create font awesome library and Icon

---
+++ src/App/components/Icon/Icon.tsx
@@ -0,0 +1,19 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faArrowLeft,
+  faArrowRight,
+  faFile,
+  faLink,
+  faSearch
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from "react";
+import { IIconProps } from "./Icon.types";
+
+export const InitializeIcons = () =>
+  library.add(faSearch, faLink, faArrowLeft, faArrowRight, faFile, fab);
+
+const Icon = (props: IIconProps) => <FontAwesomeIcon {...props} />;
+
+export default Icon;
